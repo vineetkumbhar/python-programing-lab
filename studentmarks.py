490bb2f9a16b2941780482294a31a2ef228573d8
vineetkumbhar <43986104+vineetkumbhar@users.noreply.github.com>
2018-10-23 15:58:46 +0530

    Add files via upload

---
+++ studentmarks.py
@@ -0,0 +1,18 @@
+name1=input("enter 1st student name")
+name2=input("enter 2nd student name")
+name3=input("enter 3rd student name")
+english1=input("marks of 1st student in english")
+english2=input("marks of 2nd student in english")
+english3=input("marks of 3rd student in english")
+
+science1=input("marks of 1st student in science")
+science2=input("marks of 2nd student in science")
+science3=input("marks of 3rd student in science")
+
+maths1=input("maeks of 1st student in maths")
+maths2=input("maeks of 2nd student in maths")
+maths3=input("maeks of 3rd student in maths")
+
+student1==[name1  , 'english',english1,'science',science1,'maths'maths1]
+student2=[name2  , 'english',english1,'science',science1,'maths'maths1]
+student3=[name3  , 'english',english1,'science',science1,'maths'maths1]
