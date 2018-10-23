ae1fc70e860eab258f5cb6f7d31a65f8fdb6e0bd
vineetkumbhar <43986104+vineetkumbhar@users.noreply.github.com>
2018-10-23 16:19:43 +0530

    Add files via upload

---
+++ fibonaci.py
@@ -0,0 +1,19 @@
+n=input("enter a number")
+a=0
+b=1
+l=[]
+if n<0:
+ print (" enter positive ")
+elif n==0:
+ print('0')
+elif n==1:
+ print('0,1')
+else:
+ l.append(a)
+ l.append(b)
+for i in range(n-2):
+ f=a+b
+ l.append(f)
+ a=b
+ b=f
+print(l)
