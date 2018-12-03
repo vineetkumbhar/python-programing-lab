779085b6130054d9d516780072e184a22d5148d6
vineet-kumbhar <43986104+vineetkumbhar@users.noreply.github.com>
2018-12-03 21:00:14 +0530

    Add files via upload
    Entering names of students and their marks

---
+++ marks.py
@@ -0,0 +1,23 @@
+#Marks of students
+#Name-Kanav Gupta
+#Division-M
+#Roll no.-25
+n1=input("enter 1st student name")
+n2=input("enter 2nd student name")
+n3=input("enter 3rd student name")
+e1=input("marks of 1st student in english")
+e2=input("marks of 2nd studnet in english")
+e3=input("marks of 3rd student in english")
+s1=input("marks of 1st student in science")
+s2=input("marks of 2nd student in science")
+s3=input("marks of 3rd student in science")
+m1=input("marks of 1st student in maths")
+m2=input("marks of 2nd student in maths")
+m3=input("marks of 3rd student in maths")
+student1=[n1,'english',e1,'science',s1,'maths',m1]
+student2=[n2,'english',e2,'science',s2,'maths',m2]
+student3=[n3,'english',e3,'science',s3,'maths',m3]
+student=[student1,student2,student3]
+print(student)
+print(student[1])
+
